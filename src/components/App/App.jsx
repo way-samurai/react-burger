2e06c0e39894e2b222bcce8a1bb882c4967adb3a
Alexander Goncharov <GoncharovNVCHK@yandex.ru>
2022-07-07 03:04:01 +0300

    first review

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import AppHeader from "../app-header/app-header";
 import BurgerIngredients from "../burger-ingredients/burger-ingredients";
 import BurgerConstructor from "../burger-constructor/burger-constructor";
