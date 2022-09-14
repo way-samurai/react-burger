2dffcf4e72ee657bb7b798fdffb33f69a3b1a77f
Alexander Goncharov <GoncharovNVCHK@yandex.ru>
2022-09-14 19:16:28 +0300

    feat route in app

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -7,6 +7,10 @@ import { useSelector, useDispatch } from "react-redux";
 import { getBurgerIngredients } from "../../services/actions/ingredients";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import { DndProvider } from "react-dnd";
+import { Switch, Route, useLocation, useHistory } from "react-router-dom";
+import { ProtectedRoute } from "../protected-route/protected-route";
+import { ForgotPassword, Login, NotFound404, Profile, Register, ResetPassword } from '../../pages';
+import IngredientDetails from "../ingredient-details/ingredient-details";
 
 function App() {
   const dispatch = useDispatch();
@@ -20,18 +24,48 @@ function App() {
   return (
     <div className={appStyle.app}>
       <AppHeader />
-      <main className={appStyle.main}>
-        {isLoading && "Загрузка..."}
-        {hasError && "Произошла ошибка"}
-        {!isLoading && !hasError && (
-          <DndProvider backend={HTML5Backend}>
-            <BurgerIngredients />
-            <BurgerConstructor />
-          </DndProvider>
-        )}
-      </main>
+      {/* <Switch location={background || location}> */}
+      <Switch>
+        <Route path="/" exact={true}>
+          <main className={appStyle.main}>
+            {isLoading && "Загрузка..."}
+            {hasError && "Произошла ошибка"}
+            {!isLoading && !hasError && (
+              <DndProvider backend={HTML5Backend}>
+                <BurgerIngredients />
+                <BurgerConstructor />
+              </DndProvider>
+            )}
+          </main>
+        </Route>
+        <Route path="/login" exact={true}>
+          <Login />
+        </Route>
+        <Route path="/register" exact={true}>
+          <Register />
+        </Route>
+        <Route path="/forgot-password" exact={true}>
+          <ForgotPassword />
+        </Route>
+        <Route path="/reset-password" exact={true}>
+          <ResetPassword />
+        </Route>
+        <Route path="/ingredients/:id" exact={true}>
+          <IngredientDetails />
+        </Route>
+        <ProtectedRoute path="/profile">
+          <Profile />
+        </ProtectedRoute>
+        <Route>
+          <NotFound404 />
+        </Route>
+      </Switch>
     </div>
   );
 }
 
 export default App;
+
+
+
+//Нужно проверить верстку каждой страницы!
