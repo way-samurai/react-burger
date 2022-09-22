33e0c5fd3ef93bc543fb6169f5469eb213d0e3ba
Alexander Goncharov <GoncharovNVCHK@yandex.ru>
2022-09-23 02:30:34 +0300

    feat new actions and reducers

---
+++ src/services/actions/auth.js
@@ -0,0 +1,266 @@
+import {
+  authorizationRequest,
+  registrationUserRequest,
+  getUserDataRequest,
+  updateUserDataRequest,
+  logoutRequest,
+  updateTokenRequest,
+  recoveryPasswordRequest,
+  resetPasswordRequest
+} from "../../utils/api/api";
+import { deleteCookie, getCookie, setCookie } from '../../utils/cookie/cookie';
+
+//Получение данных пользователя
+export const GET_USER_REQUEST = "GET_USER_REQUEST";
+export const GET_USER_SUCCESS = "GET_USER_SUCCESS";
+export const GET_USER_FAILED = "GET_USER_FAILED";
+
+//Обновление данных пользователя 
+export const UPDATE_USER_REQUEST = "UPDATE_USER_REQUEST";
+export const UPDATE_USER_SUCCESS = "UPDATE_USER_SUCCESS";
+export const UPDATE_USER_FAILED = "UPDATE_USER_FAILED";
+
+//Регистрация пользователя
+export const REGISTRATION_FORM_REQUEST = "REGISTRATION_FORM_REQUEST";
+export const REGISTRATION_FORM_SUCCESS = "REGISTRATION_FORM_SUCCESS";
+export const REGISTRATION_FORM_FAILED = "REGISTRATION_FORM_FAILED";
+
+//Авторизация
+export const LOGIN_REQUEST = "LOGIN_REQUEST";
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const LOGIN_FAILED = "LOGIN_FAILED";
+
+//Выход из системы
+export const LOGOUT_REQUEST = "LOGOUT_REQUEST";
+export const LOGOUT_SUCCESS = "LOGOUT_SUCCESS";
+export const LOGOUT_FAILED = "LOGOUT_FAILED";
+
+//Восстановление пароля
+export const RECOVERY_PASSWORD_REQUEST = "RECOVERY_PASSWORD_REQUEST";
+export const RECOVERY_PASSWORD_SUCCESS = "RECOVERY_PASSWORD_SUCCESS";
+export const RECOVERY_PASSWORD_FAILED = "RECOVERY_PASSWORD_FAILED";
+
+//Сброс пароля пользователя
+export const RESET_FORM_SET_VALUE = "RESET_FORM_SET_VALUE";
+export const RESET_PASSWORD_REQUEST = "RESET_PASSWORD_REQUEST";
+export const RESET_PASSWORD_SUCCESS = "RESET_PASSWORD_SUCCESS";
+export const RESET_PASSWORD_FAILED = "RESET_PASSWORD_FAILED";
+
+//Обновление токена
+export const UPDATE_TOKEN_REQUEST = "UPDATE_TOKEN_REQUEST";
+export const UPDATE_TOKEN_SUCCESS = "UPDATE_TOKEN_SUCCESS";
+export const UPDATE_TOKEN_FAILED = "UPDATE_TOKEN_FAILED";
+
+//Проверка авторизован пользователь или нет
+export const AUTH_CHECKED = "AUTH_CHECKED";
+
+//Получение данных о пользователе
+export function getUserData() {
+  return function (dispatch) {
+    dispatch({
+      type: GET_USER_REQUEST,
+    });
+    getUserDataRequest()
+      .then((res) => {
+        dispatch({
+          type: GET_USER_SUCCESS,
+          user: res.user,
+        });
+      })
+      .catch(() => {
+        dispatch({
+          type: GET_USER_FAILED,
+        });
+      });
+  };
+}
+
+//Обновление данных пользователя
+export function updateUserData(email, name, password) {
+  return function (dispatch) {
+    dispatch({
+      type: UPDATE_USER_REQUEST,
+    });
+    updateUserDataRequest(email, name, password)
+      .then((res) => {
+        dispatch({
+          type: UPDATE_USER_SUCCESS,
+          user: res.user,
+        });
+      })
+      .catch(() => {
+        dispatch({
+          type: UPDATE_USER_FAILED,
+        });
+      });
+  };
+}
+
+//Регистрация пользователя
+export function registrationUser(name, email, password) {
+  return function (dispatch) {
+    dispatch({
+      type: REGISTRATION_FORM_REQUEST,
+    });
+    registrationUserRequest(name, email, password)
+      .then((res) => {
+        const accessToken = res.accessToken.split("Bearer ")[1];
+        const refreshToken = res.refreshToken;
+        setCookie("token", accessToken);
+        localStorage.setItem("refreshToken", refreshToken);
+        return res;
+      })
+      .then((res) => {
+        dispatch({
+          type: REGISTRATION_FORM_SUCCESS,
+          user: res.user,
+        });
+      })
+      .catch(() => {
+        dispatch({
+          type: REGISTRATION_FORM_FAILED,
+        });
+      });
+  };
+}
+
+//Авторизация
+export function authorization(email, password) {
+  return function (dispatch) {
+    dispatch({
+      type: LOGIN_REQUEST,
+    });
+    authorizationRequest(email, password)
+      .then((res) => {
+        const accessToken = res.accessToken.split("Bearer ")[1];
+        const refreshToken = res.refreshToken;
+        setCookie("token", accessToken);
+        localStorage.setItem("refreshToken", refreshToken);
+        return res;
+      })
+      .then((res) => {
+        dispatch({
+          type: LOGIN_SUCCESS,
+          user: res.user,
+        });
+      })
+      .catch(() => {
+        dispatch({
+          type: LOGIN_FAILED,
+        });
+      });
+  };
+}
+
+//Выход из системы
+export function logout() {
+  return function (dispatch) {
+    dispatch({
+      type: LOGOUT_REQUEST,
+    });
+    logoutRequest()
+      .then((res) => {
+        deleteCookie("token");
+        localStorage.clear()
+        if (res && res.ok) {
+          dispatch({
+            type: LOGOUT_SUCCESS,
+          });
+        } else {
+          dispatch({ type: LOGOUT_FAILED });
+          alert("Ошибка выхода из аккаунта");
+        }
+      })
+      .catch(() => {
+        dispatch({
+          type: LOGOUT_FAILED,
+        });
+        alert("Ошибка выхода из аккаунта")
+      });
+  };
+}
+
+//Восстановление пароля
+export function recoveryPassword(email) {
+  return function (dispatch) {
+    dispatch({
+      type: RECOVERY_PASSWORD_REQUEST,
+    });
+    recoveryPasswordRequest(email)
+      .then((res) => {
+        dispatch({
+          type: RECOVERY_PASSWORD_SUCCESS,
+          message: res.message,
+        });
+      })
+      .catch(() => {
+        dispatch({
+          type: RECOVERY_PASSWORD_FAILED,
+        });
+      });
+  };
+}
+
+export const setResetFormValue = (field, value) => ({
+  type: RESET_FORM_SET_VALUE,
+  field,
+  value,
+});
+
+//Сброс пароля пользователя
+export function resetPassword(password, token) {
+  return function (dispatch) {
+    dispatch({
+      type: RESET_PASSWORD_REQUEST,
+    });
+    resetPasswordRequest(password, token)
+      .then((res) => {
+        dispatch({
+          type: RESET_PASSWORD_SUCCESS,
+        });
+      })
+      .catch(() => {
+        dispatch({
+          type: RESET_PASSWORD_FAILED,
+        });
+      });
+  };
+}
+
+//Обновление токена
+export function updateToken() {
+  return function (dispatch) {
+    dispatch({ type: UPDATE_TOKEN_REQUEST });
+    updateTokenRequest()
+      .then((res) => {
+        const authToken = res.accessToken.split("Bearer ")[1];
+        const refreshToken = res.refreshToken;
+        setCookie("token", authToken);
+        localStorage.setItem("refreshToken", refreshToken);
+        dispatch({
+          type: UPDATE_TOKEN_SUCCESS,
+        });
+      })
+      .catch((err) => {
+        dispatch({
+          type: UPDATE_TOKEN_FAILED,
+        });
+      });
+  };
+}
+
+export const checkUzerAuth = () => {
+  return function (dispatch) {
+    if (getCookie("token")) {
+      dispatch(getUserData())
+      dispatch({
+        type: AUTH_CHECKED,
+      });
+    } else {
+      dispatch({
+        type: AUTH_CHECKED,
+      });
+    }
+  };
+};
+
---
+++ src/services/reducers/auth.js
@@ -0,0 +1,361 @@
+import {
+  GET_USER_REQUEST,
+  GET_USER_SUCCESS,
+  GET_USER_FAILED,
+  UPDATE_USER_REQUEST,
+  UPDATE_USER_SUCCESS,
+  UPDATE_USER_FAILED,
+  REGISTRATION_FORM_REQUEST,
+  REGISTRATION_FORM_SUCCESS,
+  REGISTRATION_FORM_FAILED,
+  LOGIN_REQUEST,
+  LOGIN_SUCCESS,
+  LOGIN_FAILED,
+  LOGOUT_REQUEST,
+  LOGOUT_SUCCESS,
+  LOGOUT_FAILED,
+  RECOVERY_PASSWORD_REQUEST,
+  RECOVERY_PASSWORD_SUCCESS,
+  RECOVERY_PASSWORD_FAILED,
+  RESET_PASSWORD_REQUEST,
+  RESET_PASSWORD_SUCCESS,
+  RESET_PASSWORD_FAILED,
+  UPDATE_TOKEN_REQUEST,
+  UPDATE_TOKEN_SUCCESS,
+  UPDATE_TOKEN_FAILED,
+} from "../actions/auth";
+
+
+
+const initialState = {
+  isAuthSuccess: false,
+
+  form: {
+    name: "",
+    email: "",
+    password: "",
+    code: ""
+  },
+   
+  user: null,
+
+  getUserDataRequest: false,
+  getUserDataFaild: false,
+  getUserDataSuccess: false,
+
+  updateUserDataRequest: false,
+  updateUserDataFaild: false,
+  updateUserDataSuccess: false,
+
+  registrationUserRequest: false,
+  registrationUserFaild: false,
+  registrationUserSuccess: false,
+
+  authorizationRequest: false,
+  authorizationFaild: false,
+  authorizationSuccess: false,
+
+  logoutRequest: false,
+  logoutFaild: false,
+  logoutSuccess: false,
+
+  recoveryPasswordRequest: false,
+  recoveryPasswordFaild: false,
+  recoveryPasswordSuccess: false,
+  
+  resetPasswordRequest: false,
+  resetPasswordFaild: false,
+  resetPasswordSuccess: false,
+
+  updateTokenRequest: false,
+  updateTokenFaild: false,
+  updateTokenSuccess: false,
+}
+
+export const authReducer = (state = initialState, action) => {
+  switch (action.type) {
+    //Получение данных пользователя
+    case GET_USER_REQUEST: {
+      return {
+        ...state,
+
+        getUserDataRequest: true,
+        getUserDataFaild: false,
+      }
+    }
+
+    case GET_USER_FAILED: {
+      return {
+        ...state,
+
+        getUserDataRequest: false,
+        getUserDataFaild: true,
+      }
+    }
+
+    case GET_USER_SUCCESS: {
+      return {
+        ...state,
+
+        getUserDataFaild: false,
+        getUserDataSuccess: true,
+
+        user: action.user,
+      }
+    }
+
+    //Обновление данных пользователя 
+    case UPDATE_USER_REQUEST: {
+      return {
+        ...state,
+
+        updateUserDataRequest: true,
+        updateUserDataFaild: false,
+        updateUserDataSuccess: false,
+      }
+    }
+
+    case UPDATE_USER_FAILED: {
+      return {
+        ...state,
+
+        updateUserDataRequest: false,
+        updateUserDataSuccess: false,
+        updateUserDataFaild: true,
+      }
+    }
+
+    case UPDATE_USER_SUCCESS: {
+      return {
+        ...state,
+
+        updateUserDataRequest: false,
+        updateUserDataSuccess: true,
+        updateUserDataFaild: false,
+
+        user: action.user,
+
+        form: {
+          ...state.form,
+          email: "",
+          password: "",
+          name: "", 
+        },
+      }
+    }
+
+    //Регистрация пользователя
+    case REGISTRATION_FORM_REQUEST: {
+      return {
+        ...state,
+
+        registrationUserRequest: true,
+        registrationUserFaild: false,
+        registrationUserSuccess: false,
+      }
+    }
+
+    case REGISTRATION_FORM_FAILED: {
+      return {
+        ...state,
+
+        registrationUserRequest: false,
+        registrationUserFaild: true,
+        registrationUserSuccess: false,
+      }
+    }
+
+    case REGISTRATION_FORM_SUCCESS: {
+      return {
+        ...state,
+
+        user: action.user,
+
+        form: {
+          ...state.form,
+          email: "",
+          password: "",
+          name: "",
+        },
+
+        registrationUserRequest: false,
+        registrationUserFaild: false,
+        registrationUserSuccess: true,
+      }
+    }
+
+    case LOGIN_REQUEST: {
+      return {
+        authorizationRequest: true,
+        authorizationFaild: false,
+        authorizationSuccess: false,
+      }
+    }
+
+    case LOGIN_FAILED: {
+      return {
+        authorizationRequest: false,
+        authorizationFaild: true,
+        authorizationSuccess: false,
+      
+        isAuthSuccess: false,
+      }
+    }
+
+    case LOGIN_SUCCESS: {
+      
+      return {
+        ...state,
+
+        user: action.user,
+
+        form: {
+          ...state.form,
+          email: '',
+          name: '',
+          password: '',
+        },
+
+        
+        isAuthSuccess: true,
+
+        authorizationRequest: false,
+        authorizationFaild: false,
+        authorizationSuccess: true,
+      }
+    }
+
+    //Выход из системы
+    case LOGOUT_REQUEST: {
+      return {
+        logoutRequest: true,
+        logoutFaild: false,
+        logoutSuccess: false,
+      }
+    }
+
+    case LOGOUT_FAILED: {
+      return {
+        logoutRequest: false,
+        logoutFaild: true,
+        logoutSuccess: false,
+      }
+    }
+
+    case LOGOUT_SUCCESS: {
+      return {
+        ...state,
+        user: null,
+
+        form: {
+          ...state.form,
+          email: '',
+          name: '',
+          password: '',
+        },
+
+        logoutRequest: false,
+        logoutFaild: false,
+        logoutSuccess: true,
+
+        isAuthSuccess: false,
+      }
+    }
+
+    //Восстановление пароля
+    case RECOVERY_PASSWORD_REQUEST: {
+      return {
+        recoveryPasswordRequest: true,
+        recoveryPasswordFaild: false,
+        recoveryPasswordSuccess: false,
+      }
+    }
+
+    case RECOVERY_PASSWORD_FAILED: {
+      return {
+        recoveryPasswordRequest: false,
+        recoveryPasswordFaild: true,
+        recoveryPasswordSuccess: false,
+      }
+    }
+
+    case RECOVERY_PASSWORD_SUCCESS: {
+      return {
+        ...state,
+
+        form: { //ПРОВЕРИТЬ
+          ...state.form,
+          email: '',
+        },
+
+        message: action.message,
+
+        recoveryPasswordRequest: false,
+        recoveryPasswordFaild: false,
+        recoveryPasswordSuccess: true,
+      }
+    }
+    
+
+    //Сброс пароля
+    case RESET_PASSWORD_REQUEST: {
+      return {
+        ...state,
+
+        resetPasswordRequest: true,
+        resetPasswordFaild: false,
+        resetPasswordSuccess: false,
+      }
+    }
+
+    case RESET_PASSWORD_FAILED: {
+      return {
+        ...state,
+
+        resetPasswordRequest: false,
+        resetPasswordFaild: true,
+        resetPasswordSuccess: false,
+      }
+    }
+
+    case RESET_PASSWORD_SUCCESS: {
+      return {
+        ...state,
+
+        resetPasswordRequest: false,
+        resetPasswordFaild: false,
+        resetPasswordSuccess: true,
+      }
+    }
+    
+    //Обновление токена
+    case UPDATE_TOKEN_REQUEST: {
+      return {
+        updateTokenRequest: true,
+        updateTokenFaild: false,
+        updateTokenSuccess: false,
+      }
+    }
+
+    case UPDATE_TOKEN_FAILED: {
+      return {
+        updateTokenRequest: false,
+        updateTokenFaild: true,
+        updateTokenSuccess: false,
+      }
+    }
+
+    case UPDATE_TOKEN_SUCCESS: {
+      return {
+        updateTokenRequest: false,
+        updateTokenFaild: false,
+        updateTokenSuccess: true,
+      }
+    }
+
+    default: {
+      return state;
+    }
+  }
+
+}
